e2ad6f8653985f3c851375f244a4b89d885fcc41
MigMikael <chanachai_mig@hotmail.com>
2018-10-29 15:42:54 +0700

    divide disjoint data

--- divide_data.py
+++ divide_data.py
@@ -1,94 +1,91 @@
 from random import shuffle
+from collections import defaultdict
 
 train_ratio = 60
 test_ratio = 20
 vali_ratio = 20
 folder_name = "data_2"
 
-device_data = "D:\\Documents\\Thesis\\" + folder_name + "\\iPad_MotoC_plus_data_7.txt"
-output_train_data = "D:\\Documents\\Thesis\\" + folder_name + "\\iPad_MotoC_plus_train_data_6.txt"
-output_test_data = "D:\\Documents\\Thesis\\" + folder_name + "\\iPad_MotoC_plus_test_data_6.txt"
-output_vali_data = "D:\\Documents\\Thesis\\" + folder_name + "\\iPad_MotoC_plus_vali_data_6.txt"
+device_data = "D:\\Documents\\Thesis\\" + folder_name + "\\S8_MotoC_plus_data_7.txt"
+output_train_data = "D:\\Documents\\Thesis\\" + folder_name + "\\S8_MotoC_plus_train_data_7.txt"
+output_test_data = "D:\\Documents\\Thesis\\" + folder_name + "\\S8_MotoC_plus_test_data_7.txt"
+output_vali_data = "D:\\Documents\\Thesis\\" + folder_name + "\\S8_MotoC_plus_vali_data_7.txt"
 
 data_file = open(device_data, "r")
 data_list = data_file.readlines()
-print("Finish Read File")
+shuffle(data_list)
+print("### Finish Read File")
 
-#shuffle(data_list)
-print("Finish Shuffle data")
-
-data_dict = {}
-
+all_list = []
 for item in data_list:
     item = item.replace("\n", "")
     item = item.replace("|labels ", "")
     item = item.replace("features", "")
 
     label, feature = item.split(" | ")
-    # print(label, "=", feature)
+    all_list.append((feature, label))
 
-    data_dict[feature] = label
-
-print(len(data_dict))
-len_data_dict = len(data_dict)
-len_collect = int(0.2 * len_data_dict)
-print(len_collect)
-
-feature_collect = []
+print(all_list[:3])
+d = defaultdict(list)
+for f, l in all_list:
+    d[f].append(l)
 
 count = 0
-for key in data_dict:
-    feature_collect.append("|features " + key)
-    count += 1
-    if count <= 5:
-        print(key, "=>", data_dict[key])
-
-    if count == len_collect:
+for key in d:
+    if count == 5:
         break
-
-print(len(feature_collect))
-print(feature_collect[:5])
-
-count = 0
-vali_list = []
-for key in data_dict:
-    if count == len_collect:
-        break
-    line = "|labels " + data_dict[key] + " |features " + key + "\n"
-    vali_list.append(line)
+    print(key, "=>", d[key])
     count += 1
 
-print(len(vali_list))
-print(vali_list[:5])
-count = 0
+length = len(d)
+print("Unique Feature : ", length)
 
-data_list = [e for e in data_list if e not in feature_collect]
-
-print(len(data_list))
-'''
-length = len(data_list)
 #print(length)
 train_length = int(length * (train_ratio/100))
 test_length = int(length * (test_ratio/100))
 vali_length = int(length * (vali_ratio/100))
-print("Train : ", train_length)
-print("Test : ", test_length)
-print("Vali : ", vali_length)
-print("Finish Divide data")
+print("Unique Train : ", train_length)
+print("Unique Test : ", test_length)
+print("Unique Vali : ", vali_length)
 
-print("Train Length", train_length)
-print("Test Length", test_length)
-print("Validata Length", vali_length)
+count = 0
+count_total = 0
 
-with open(output_train_data, "a") as train_file:
-    for i in range(train_length):
-        line = data_list[i]
-        train_file.write(line)
-print("Finish Write Train File")
+vali_list = []
+train_test_list = []
+
+for key in d:
+    for item in d[key]:
+        line = "|label " + item + " |features " + key + "\n"
+        if count <= vali_length:
+            vali_list.append(line)
+        else:
+            train_test_list.append(line)
+    count += 1
+print("### Finish Divide Data")
+
+shuffle(vali_list)
+shuffle(train_test_list)
+print("### Finish Shuffle data")
+
+print("vali_list : ", len(vali_list))
+#print("train_test_list : ", len(train_test_list))
+test_list = train_test_list[:len(vali_list)]
+train_list = train_test_list[len(vali_list):]
+print("test_list : ", len(test_list))
+print("train_list : ", len(train_list))
+
+with open(output_vali_data, "a") as vali_file:
+    for line in vali_list:
+        vali_file.write(line)
+print("### Finish Write Validate File")
 
 with open(output_test_data, "a") as test_file:
-    for i in range(test_length):
-        line = data_list[train_length + i]
+    for line in test_list:
         test_file.write(line)
-print("Finish Write Test File")
-'''
+print("### Finish Write Test File")
+
+with open(output_train_data, "a") as train_file:
+    for line in train_list:
+        train_file.write(line)
+print("### Finish Write Train File")
